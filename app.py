195773f101166700162f256270df0c49bb3d05bb
Rishav <rishavdadwal1@gmail.com>
2023-08-11 23:23:20 +0530

    making

--- app.py
+++ app.py
@@ -59,18 +59,20 @@ ending_response = [
     "So long! Looking forward to our next conversation.",
     "Until we chat again, goodbye and stay awesome!"
 ]
+user_asking = [
+    "how are you",
+    "how's it going",
+    "what's up",
+    "how's everything",
+    "are you doing well",
+    "how are things on your end",
+    "are you ready to help",
+    "are you available",
+    "how are you today"
+]
 
 goodbye_keywords = ["bye", "goodbye", "see you", "farewell", "adios", "au revoir", "ciao"]
-def greeting(sentences):
-    for i in sentences.split():
-        if i.lower() in greeting_inputs:
-            return random.choice(greeting_responses)
-def ending(senctences):
-    for i in senctences.split():
-        if i.lower() in goodbye_keywords:
-            return "bye"
 
-# Vectorizer
 chatbot_unaware = [
     "I'm sorry, but I can't answer that.",
     "Unfortunately, I can't provide an answer to that.",
@@ -78,6 +80,50 @@ chatbot_unaware = [
     "I apologize, but I can't answer that question.",
     "Regrettably, I can't provide an answer to that."
 ]
+chatbot_thanks_response = [
+    "You're welcome! Feel free to ask more questions.",
+    "Glad to assist! If you need more info, ask away.",
+    "Happy to help. Have more queries? Just ask.",
+    "No problem! Reach out if you want more insights.",
+    "My pleasure! Any more questions? Let me know.",
+    "You're welcome! More info available on request.",
+    "Anytime! Need further assistance? Just ask.",
+    "Glad I could assist! Keep the questions coming.",
+    "No worries! Want to know more? Just ask."
+]
+
+asking_response = [
+    "I'm here and ready to assist you!",
+    "I'm just a chatbot, but I'm here to help!",
+    "I'm functioning as expected and here to provide information.",
+    "I'm a machine, so I don't have feelings, but I'm at your service!",
+    "I'm here, waiting to answer your questions!",
+    "I'm here and fully operational to help with any inquiries.",
+    "I don't have feelings, but I'm here and eager to assist you.",
+    "I'm here to provide information and support!",
+    "I'm a chatbot, so I'm always ready to help you out!",
+    "I don't experience emotions, but I'm available to help you.",
+    "I'm here, powered up, and ready to answer your queries!",
+    "I'm doing well in terms of functionality. How can I assist you today?",
+    "I'm here to help, so feel free to ask me anything you need.",
+    "I'm ready and equipped to provide you with information.",
+    "I'm just a program, but I'm operational and ready to assist you!"
+]
+
+def greeting(sentences):
+    for i in sentences.split():
+        if i.lower() in greeting_inputs:
+            return random.choice(greeting_responses)
+def ending(sentences):
+    for i in sentences.split():
+        if i.lower() in goodbye_keywords:
+            return "bye"
+def asking(sentences):
+    for i in sentences.split():
+        if i.lower() in user_asking:
+            return random.choice(asking_response)
+
+# Vectorizer
 
 
 
@@ -99,21 +145,26 @@ def response(user_response):
         chatbot_response = chatbot_response + sent_tokens[idx]
         return chatbot_response
 
-if __name__=="__main__":
+if __name__ == "__main__":
     flag = True
     print("Hello! I am your Portfolio Companion Chatbot created by Rishav.")
-    while(flag==True):
+    
+    while flag:
         user_response = input()
         user_response = user_response.lower()
-        if(ending(user_response)!= "bye"):
+        
+        if ending(user_response) != "bye":
             if user_response == 'thanks' or user_response == 'thank you':
                 flag = False
-                print("Bot: You' re welcome!")
+                print(random.choice(chatbot_thanks_response))
+                break
+            elif asking(user_response) is not None:
+                print("Bot: " + asking(user_response))
             else:
-                if greeting(user_response)!=None:
+                if greeting(user_response) is not None:
                     print("Bot: " + greeting(user_response))
                 else:
-                    print("Bot:",end='')
+                    print("Bot:", end='')
                     print(response(user_response))
                     sent_tokens.remove(user_response)
         else:
